20bdeeca4b3fbb867d6ad846643a9e388127e28b
ferinaanjani <anjaniferina97@gmal.com>
2019-08-08 16:50:18 +0700

    fixing login and other

--- app/src/main/java/com/example/cx62/rpk/MainActivity.java
+++ app/src/main/java/com/example/cx62/rpk/MainActivity.java
@@ -60,30 +60,18 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
         setContentView(R.layout.activity_main);
 
 
-
         Device_ID = findViewById(R.id.Device_ID);
         inputNIK = findViewById(R.id.inputNIK);
         inputPassword = findViewById(R.id.inputPassword);
 
-        Latitude = (TextView)findViewById(R.id.Latitude);
-        Longitude = (TextView)findViewById(R.id.Longitude);
+        Latitude = (TextView) findViewById(R.id.Latitude);
+        Longitude = (TextView) findViewById(R.id.Longitude);
 
 
-
-        ActivityCompat.requestPermissions(MainActivity.this,
-                new String[]{Manifest.permission.READ_PHONE_STATE},
-                1);
+        ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.READ_PHONE_STATE},1);
 
 
-
-
-
-
-        googleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
+        googleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
@@ -92,46 +80,43 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
             @Override
             public void onClick(View v) {
 
-                Buttona.setBackgroundColor(Color.rgb(54, 53, 59));
+                Buttona.setBackgroundColor(Color.rgb(54,53,59));
                 Buttona.setEnabled(false);
 
 
+                String NIK = inputNIK.getText().toString();
+                String Password = inputPassword.getText().toString();
+                final String devid = Device_ID.getText().toString();
 
-                    String NIK = inputNIK.getText().toString();
-                    String Password = inputPassword.getText().toString();
-                    final String devid = Device_ID.getText().toString();
-
-                    Call<ResponBodyKaryawan> callLogin = RetrofitClient.getInstance().getAPI().login(NIK,Password,devid);
-                    callLogin.enqueue(new Callback<ResponBodyKaryawan>() {
-                        @Override
-                        public void onResponse(Call<ResponBodyKaryawan> call, Response<ResponBodyKaryawan> response) {
-                            if (response.code() == 200){
-                                HasilLogin.Device_ID = devid;
-                                HasilLogin.ID_Karyawan = response.body().data.ID_Karyawan;
-                                HasilLogin.NIK = response.body().data.getNIK();
-                                HasilLogin.lat = ""+lat;
-                                HasilLogin.lng = ""+lng;
-                                Intent i = new Intent(MainActivity.this, dashboard.class);
-                                startActivity(i);
-                            }else if (response.code() == 201){
-                                Toast.makeText(MainActivity.this, "Anda Masih Berstatus Pending", Toast.LENGTH_SHORT).show();
-                            }
-                            else{
-                                Toast.makeText(MainActivity.this, "Gagal", Toast.LENGTH_SHORT).show();
-                            }
-                            Buttona.setEnabled(true);
-                            Buttona.setBackgroundColor(Color.rgb(214, 118, 1));
-
+                Call<ResponBodyKaryawan> callLogin = RetrofitClient.getInstance().getAPI().login(NIK,Password,devid);
+                callLogin.enqueue(new Callback<ResponBodyKaryawan>() {
+                    @Override
+                    public void onResponse(Call<ResponBodyKaryawan> call,Response<ResponBodyKaryawan> response) {
+                        if (response.code() == 200) {
+                            HasilLogin.Device_ID = devid;
+                            HasilLogin.ID_Karyawan = response.body().data.ID_Karyawan;
+                            HasilLogin.NIK = response.body().data.getNIK();
+                            HasilLogin.lat = "" + lat;
+                            HasilLogin.lng = "" + lng;
+                            Intent i = new Intent(MainActivity.this,dashboard.class);
+                            startActivity(i);
+                        } else if (response.code() == 201) {
+                            Toast.makeText(MainActivity.this,"Anda Masih Berstatus Pending",Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this,"Gagal",Toast.LENGTH_SHORT).show();
                         }
+                        Buttona.setEnabled(true);
+                        Buttona.setBackgroundColor(Color.rgb(214,118,1));
 
-                        @Override
-                        public void onFailure(Call<ResponBodyKaryawan> call, Throwable t) {
-                            Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
-                            Buttona.setEnabled(true);
-                            Buttona.setBackgroundColor(Color.rgb(214, 118, 1));
-                        }
-                    });
+                    }
 
+                    @Override
+                    public void onFailure(Call<ResponBodyKaryawan> call,Throwable t) {
+                        Toast.makeText(MainActivity.this,t.getMessage(),Toast.LENGTH_SHORT).show();
+                        Buttona.setEnabled(true);
+                        Buttona.setBackgroundColor(Color.rgb(214,118,1));
+                    }
+                });
 
 
             }
@@ -141,16 +126,17 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
         Buttonb.setOnClickListener(new View.OnClickListener() {
                                        @Override
                                        public void onClick(View v) {
-                                           Intent i = new Intent(MainActivity.this, registrasi.class);
-                                           i.putExtra("Device_ID", Device_ID.getText().toString());
-                                           i.putExtra("Latitude", ""+lat);
-                                           i.putExtra("Longitude", ""+lng);
+                                           Intent i = new Intent(MainActivity.this,registrasi.class);
+                                           i.putExtra("Device_ID",Device_ID.getText().toString());
+                                           i.putExtra("Latitude","" + lat);
+                                           i.putExtra("Longitude","" + lng);
                                            startActivity(i);
                                        }
                                    }
 
         );
     }
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -165,58 +151,66 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
         super.onStop();
 
     }
+
     @Override
     public void onConnectionSuspended(int i) {
-        Log.e("MainActivity", "Connection Suspended");
+        Log.e("MainActivity","Connection Suspended");
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Log.e("MainActivity", "Connection Failed" + connectionResult.getErrorCode());
+        Log.e("MainActivity","Connection Failed" + connectionResult.getErrorCode());
     }
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             requestPermission();
-        }else{
-            fusedLocationProviderClient.getLastLocation()
-                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
+        } else {
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this,new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
 
-                          lng = location.getLongitude();
-                          lat=location.getLatitude();
-                            Latitude.setText(""+lng);
-                            Longitude.setText(""+lat);
+                    lng = location.getLongitude();
+                    lat = location.getLatitude();
+                    Latitude.setText("" + lng);
+                    Longitude.setText("" + lat);
 
 
-                        }
-                    });
+                }
+            });
         }
 
 
     }
 
-    private void requestPermission(){
-        ActivityCompat.requestPermissions(MainActivity.this, new
-                String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, RequestPermissionCode);
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(MainActivity.this,new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},RequestPermissionCode);
 
     }
 
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    public void onRequestPermissionsResult(int requestCode,@NonNull String[] permissions,@NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
         switch (requestCode) {
             case 1: {
 
                 // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
                     TelephonyManager tm = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+                    if (ActivityCompat.checkSelfPermission(this,Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
                     Device_ID.setText(tm.getDeviceId());
 
                     // permission was granted, yay! Do the
--- app/src/main/java/com/example/cx62/rpk/RetrofitClient.java
+++ app/src/main/java/com/example/cx62/rpk/RetrofitClient.java
@@ -1,18 +1,26 @@
 package com.example.cx62.rpk;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
+
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
-    String BASE_URL = "http://192.168.86.15/Laravel/pkl/public/api/";
+    String BASE_URL = "http://192.168.43.169/absensi/pkl/public/api/";
     private static RetrofitClient mInstance;
     private Retrofit retrofit;
 
 
     public RetrofitClient(){
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
         retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
